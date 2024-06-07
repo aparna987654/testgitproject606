329454a822c9d2cc1d621ab43c0892cf0119e6ad
KCP <KCP@DESKTOP-5JB5VIM>
2024-06-07 14:27:39 +0530

    OK

---
+++ PetstoreAutomation/src/test/java/Homelyf/GET_User.java
@@ -0,0 +1,84 @@
+package Homelyf;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class GET_User {
+  @Test
+  public static void lookupcategory() {
+	  RestAssured.baseURI="https://homelyfservices.com/userAPI/Swagger/index.html";
+	  RequestSpecification httpRequest=RestAssured.given().log().all();
+	  Response response=httpRequest.request(Method.GET,"https://homelyfservices.com/UserAPI/api/Lookup/Category");
+	  
+	  String responsebody=response.getBody().asString();
+	  int statuscode=response.statusCode();
+	  
+	  Assert.assertEquals(statuscode, 200);
+	  System.out.println("response body is:" +responsebody);
+	  System.out.println("status code is:" +statuscode);
+  }
+  
+  @Test
+    public static void lookupCountry() {
+	  RestAssured.baseURI="https://homelyfservices.com/userAPI/Swagger/index.html";
+	  RequestSpecification httpRequest=RestAssured.given().log().all();
+	  Response response=httpRequest.request(Method.GET,"https://homelyfservices.com/UserAPI/api/Lookup/Country");
+	  
+	  String responsebody=response.getBody().asString();
+	  int statuscode=response.statusCode();
+	  
+	  Assert.assertEquals(statuscode, 200);
+	  System.out.println("response body is:" +responsebody);
+	  System.out.println("status code is:" +statuscode);
+  }
+	  
+  @Test
+  public static void lookupState() {
+	  RestAssured.baseURI="https://homelyfservices.com/userAPI/Swagger/index.html";
+	  RequestSpecification httpRequest=RestAssured.given().log().all();
+	  Response response=httpRequest.request(Method.GET,"https://homelyfservices.com/UserAPI/api/Lookup/State");
+	  
+	  String responsebody=response.getBody().asString();
+	  int statuscode=response.statusCode();
+	  
+	  Assert.assertEquals(statuscode, 200);
+	  System.out.println("response body is:" +responsebody);
+	  System.out.println("status code is:" +statuscode);
+}
+	  
+  
+  @Test
+  public static void lookupcity() {
+	  RestAssured.baseURI="https://homelyfservices.com/userAPI/Swagger/index.html";
+	  RequestSpecification httpRequest=RestAssured.given().log().all();
+	  Response response=httpRequest.request(Method.GET,"https://homelyfservices.com/UserAPI/api/Lookup/City");
+	  
+	  String responsebody=response.getBody().asString();
+	  int statuscode=response.statusCode();
+	  
+	  Assert.assertEquals(statuscode, 200);
+	  System.out.println("response body is:" +responsebody);
+	  System.out.println("status code is:" +statuscode);
+}
+	  
+  
+  
+  @Test
+  public static void lookuppostcode() {
+	  RestAssured.baseURI="https://homelyfservices.com/userAPI/Swagger/index.html";
+	  RequestSpecification httpRequest=RestAssured.given().log().all();
+	  Response response=httpRequest.request(Method.GET,"https://homelyfservices.com/UserAPI/api/Lookup/PostCode");
+	  
+	  String responsebody=response.getBody().asString();
+	  int statuscode=response.statusCode();
+	  
+	  Assert.assertEquals(statuscode, 200);
+	  System.out.println("response body is:" +responsebody);
+	  System.out.println("status code is:" +statuscode);
+}
+  }
+
